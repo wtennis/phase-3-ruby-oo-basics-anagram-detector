e5266fd2fa30bb47d166930a01c864d537664c09
Whiting <whitinghtennis@gmail.com>
2021-08-10 10:06:55 -0700

    commit tree 08fa2d0e393c1e0d1fd2ee59ade1bc0af6e7ca3c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        puts "I am the word #{@word.upcase}"
+        puts "the arr to check is #{arr}"
+        puts "#{@word.chars.sort}"
+        arr.select {|possible_anagram| possible_anagram.chars.sort == @word.chars.sort}
+    end
+end
+
+listen = Anagram.new("listen")
+listen.match(%w[enlists google inlets netsil banana])
